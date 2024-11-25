9bf9e09c19a8413ad511a0d5c962d542ffb6a712
Ankit Rajput <a7949@axtria.com>
2024-11-25 11:35:38 +0530

    Fast-Api

---
+++ fast-API/main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from pydantic import BaseModel
+import json
+from LinearRegression import LinearRegression
+from datetime import datetime
+from fastapi import File, UploadFile, Form
+from typing import Optional
+import csv
+from io import StringIO
+
+app=FastAPI()
+# Allow React app on localhost:3000 to communicate with the FastAPI backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Can replace with specific frontend URL, like ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods like GET, POST, etc.
+    allow_headers=["*"],  # Allows all headers
+)
+ 
+
+ 
+ 
+
+    
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+
+@app.post("/upload")
+async def forecast_sales(file:UploadFile=File(...), historyFromDate: Optional[str] = Form(None), historyToDate: Optional[str] = Form(None), selectedFromDate: Optional[str] = Form(None), selectedToDate: Optional[str] = Form(None)): 
+    contents = await file.read()
+    decoded = contents.decode('utf-8')
+    csv_reader = csv.reader(StringIO(decoded), delimiter=',')
+    data = [row for row in csv_reader]
+    forecast_val, dt = LinearRegression(data, historyFromDate, historyToDate, selectedFromDate, selectedToDate)
+    print(dt)
+    print(forecast_val)
+    return {"forecast": forecast_val, "dt": dt,"filename": file.filename, "historyFromDate" : historyFromDate,"historyToDate" : historyToDate,"selectedFromDate" : selectedFromDate,"selectedToDate" : selectedToDate}
